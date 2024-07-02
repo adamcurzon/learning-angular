6908810a82214145510ebef6399f494454d2f592
acurzon <adam@curzon.org>
2024-07-02 11:06:36 +0100

    setup: app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SnackbarComponent } from './snackbar/snackbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, RouterModule, SnackbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'learning-angular';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'My Cars Page',
+  },
+  {
+    path: 'car/:id',
+    component: CarDetailsComponent,
+    title: 'My Car Page',
+  },
+];
